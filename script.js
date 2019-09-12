9c73657115d1a15211b076fa96d67d31616b4a07
Edi Sogorovic <soed19bz@student.ju.segit config --global user.email soed19bz@student.ju.se>
2019-09-12 10:06:36 +0200

    Correcting a misstake in the age settings

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 function CheckAge(){
     var age = document.getElementById ("textbox");
 
-    if(age.value <= 18 && age.value >= 0){
+    if(age.value <= 17 && age.value >= 0){
         alert("Too young!")
     }
 
     else if(age.value > 65){
         alert("Too old!")
     }
-    else if(age.value >= 19 && age.value <=65){
+    else if(age.value >= 18 && age.value <=65){
         alert("Correct age!")
     }
 
