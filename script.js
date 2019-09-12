86533b447c9602058813d4ce4c1d167b345c3d1d
Edi Sogorovic <soed19bz@student.ju.segit config --global user.email soed19bz@student.ju.se>
2019-09-12 10:02:43 +0200

    Genomfört övning IF

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function CheckAge(){
+    var age = document.getElementById ("textbox");
+
+    if(age.value <= 18 && age.value >= 0){
+        alert("Too young!")
+    }
+
+    else if(age.value > 65){
+        alert("Too old!")
+    }
+    else if(age.value >= 19 && age.value <=65){
+        alert("Correct age!")
+    }
+
+    }
+
+
+
