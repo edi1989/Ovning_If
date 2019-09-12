ed397ef486c1e0a034a79caaf713a52afad58b15
Edi Sogorovic <soed19bz@student.ju.segit config --global user.email soed19bz@student.ju.se>
2019-09-12 10:09:24 +0200

    Added comments to script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-
+//Funktion kontrollera ålder
 function CheckAge(){
-    var age = document.getElementById ("textbox");
+    var age = document.getElementById ("textbox"); //Hämtar värdet i textrutan i index
 
-    if(age.value <= 17 && age.value >= 0){
+    if(age.value <= 17 && age.value >= 0){ //När ålder är mellan 0-17 = För ung
         alert("Too young!")
     }
 
-    else if(age.value > 65){
+    else if(age.value > 65){ //När ålder är större än 65 = För gammal
         alert("Too old!")
     }
-    else if(age.value >= 18 && age.value <=65){
+    else if(age.value >= 18 && age.value <=65){ //När ålder är mellan 18-65 = Korrekt ålder
         alert("Correct age!")
     }
 
